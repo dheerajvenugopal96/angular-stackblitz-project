5c6aec99a7b29f3fe6ad3d0c6153673aa492a8fa
dheerajvenugopal96 <dheerajvenugopal96@gmail.com>
2021-01-05 14:01:35 +0530

    3 files modified, 1 added

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,14 +1,14 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { tap, filter, map } from 'rxjs/operators';
-import { IProduct } from './products';
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { tap, filter, map } from "rxjs/operators";
+import { IProduct } from "./products";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root"
 })
 export class ProductService {
-  private productApi = '../../api/products.json';
+  private productApi = "../../assets/products.json";
   constructor(private _http: HttpClient) {}
 
   getProducts(): Observable<IProduct[]> {
@@ -17,7 +17,7 @@ export class ProductService {
 
   getProductForId(id: number): Observable<IProduct> {
     return this.getProducts().pipe(
-      map((pro: IProduct[]) => pro.find((p) => p.productId == id))
+      map((pro: IProduct[]) => pro.find(p => p.productId == id))
     );
   }
 }
